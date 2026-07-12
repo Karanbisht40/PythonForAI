73923978c88359500e6d121173b019c045376424
Karan Bisht <karanbisht4041@gmail.com>
2026-07-12 21:18:48 +0530

    AI

--- Python-for-AI/Day3Loop/Example.py
+++ Python-for-AI/Day3Loop/Example.py
@@ -1,3 +1,24 @@
-fruits = ["apple", "peach", "pear"]
-for fruit in fruits:
-    print(fruit)
+# #for loop in lists
+# fruits = ["apple", "peach", "pear"]
+# for fruit in fruits:
+#     print(fruit)
+ 
+#  #for loop with range
+# for i in range(1,10):
+#     print(i)
+
+# #print even number
+# for i in range (2, 11, 2):
+#     print(i)
+
+# total =0
+# for number in range(1,101):
+#     total += number
+#     print(total)
+
+#even sum 
+even_sum = 0
+for number in range (2, 101, 2):
+    even_sum +=number
+
+print(even_sum)
