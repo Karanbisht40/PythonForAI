39c8a129d1eb66bdc28f216750b41295be9e8a1e
Karan Bisht <karanbisht4041@gmail.com>
2026-07-14 00:25:52 +0530

    Python

---
+++ Python-for-AI/OOPs/Encapsulation.py
@@ -0,0 +1,15 @@
+
+#Encapsulation means wrapping data (variables) and methods
+#(functions) into a single unit (class) and restricting direct access to the data.
+class Student:
+
+    def __init__(self):
+        self.name = "Karan"
+        self.__marks = 95   # Private variable
+
+    def show_marks(self):
+        print(self.__marks)
+
+student = Student()
+
+student.show_marks()
