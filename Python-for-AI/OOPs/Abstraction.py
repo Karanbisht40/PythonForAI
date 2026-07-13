456b1997367976d9de37ad6b926cd6fd85e6eff2
Karan Bisht <karanbisht4041@gmail.com>
2026-07-14 00:26:43 +0530

    Python

---
+++ Python-for-AI/OOPs/Abstraction.py
@@ -0,0 +1,8 @@
+#Abstraction means hiding unnecessary implementation details and showing only the essential features.
+class Car:
+
+    def start(self):
+        print("Car Started")
+
+car = Car()
+car.start()
