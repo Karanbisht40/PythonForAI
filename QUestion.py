dc004afb1b4f82ed4b543dbad6ab97a2bf46ef09
Karan Bisht <karanbisht4041@gmail.com>
2026-07-14 23:16:11 +0530

    file

--- QUestion.py
+++ QUestion.py
@@ -1,21 +1,29 @@
-#write a program to read a text from a given file certificate.txt and find wheather it contains the word live
+#ques-write a program to read a text from a given file certificate.txt and find wheather it contains the word live
 
 #open 
-# file = open("certificate.txt", "r")
+file = open("certificate.txt", "r")
 
 # #read
-# containvalue=  file.read()
+containvalue=  file.read()
 
-# containvalue = containvalue.lower() #lower case
+containvalue = containvalue.lower() #lower case
 
-# if"live" in containvalue:
-#     print("yes live word is present in the file")
-# else:
-#     print("not contain any live word")
+if"live" in containvalue:
+    print("yes live word is present in the file")
+else:
+    print("not contain any live word")
 
  #open a file called report.txt in write mode
-
-
-#
 file = open("report.txt", "w")
 file.write("this is a demo text that inserted through the code")
+
+#ques-read only the first line of the textfile.txt
+with open("textfile.txt","r") as f:
+    data = f.readline()
+    print("data is :", data)
+    f.close()
+
+#ques- print how many lines are present in textfile.txt
+with open("textfile.txt", "r") as f:
+    listsofLines = f.readlines()
+    print("the total number of lines are", len(listsofLines))
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,5 +1,38 @@
-import os
-# file_path = os.path.join(os.path.dirname(__file__), "mydata.txt")
-# with open(file_path, "r") as file:
-#     data =  file.read()
-# print("data is:", data)
+#reading entire file
+# #normal reading
+# file = open("ex.txt", "r")
+# data= file.read()
+# print(data)
+# file.close()
+
+# #with keyword
+# with open("ex.txt", "r") as f: 
+#     data= f.read()
+#     print("file data", data)
+
+# #reading line by line   
+# with open("textfile.txt", "r") as f:
+#      line1= f.readline()
+#      line2= f.readline()
+#      line3= f.readline()
+#      line4= f.readline()
+#      line5= f.readline()
+#      line6= f.readline()
+#      data = f.read()
+#      print("line 1", line1)
+#      print("line 2", line2)
+#      print("line 3", line5)
+#      print("line 6", line6)
+#      print("data is", data)
+
+# #read all Lines 
+# with open("textfile.txt", "r") as f:
+#  lines = f.readlines()
+#  print(lines)
+
+#  #RENAME FILES
+#  import os
+#  os.rename("ex.txt", "xe.txt")
+
+#  #delete files
+# os.remove("xe.txt")
