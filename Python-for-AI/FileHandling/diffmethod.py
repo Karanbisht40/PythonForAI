d6d393c887bd06bfab9d13a0f587c4b0f1c7998f
Karan Bisht <karanbisht4041@gmail.com>
2026-07-15 00:15:27 +0530

    file

--- Python-for-AI/FileHandling/diffmethod.py
+++ Python-for-AI/FileHandling/diffmethod.py
@@ -1,7 +1,12 @@
 
 
-import os
-file_path = os.path.join(os.path.dirname(__file__), "mydata.txt")
-with open(file_path, "r") as file:
-    data =  file.read()
-print("data is:", data)
+# import os
+# file_path = os.path.join(os.path.dirname(__file__), "mydata.txt")
+# with open(file_path, "r") as file:
+#     data =  file.read()
+# print("data is:", data)
+
+file = open("./Python-for-AI/fileHandling/mydata.txt", "r")
+data= file.read()
+print(data)
+file.close()
--- ReadingFiles.py
+++ ReadingFiles.py
@@ -1,41 +1,41 @@
-# #reading entire file
-# #normal reading
-# file = open("ex.txt", "r")
-# data= file.read()
-# print(data)
-# file.close()
+#reading entire file
+#normal reading
+file = open("ex.txt", "r")
+data= file.read()
+print(data)
+file.close()
 
-# #with keyword
-# with open("ex.txt", "r") as f: 
-#     data= f.read()
-#     print("file data", data)
+#with keyword
+with open("ex.txt", "r") as f: 
+    data= f.read()
+    print("file data", data)
 
-# #reading line by line   
-# with open("textfile.txt", "r") as f:
-#      line1= f.readline()
-#      line2= f.readline()
-#      line3= f.readline()
-#      line4= f.readline()
-#      line5= f.readline()
-#      line6= f.readline()
-#      data = f.read()
-#      print("line 1", line1)
-#      print("line 2", line2)
-#      print("line 3", line5)
-#      print("line 6", line6)
-#      print("data is", data)
+#reading line by line   
+with open("textfile.txt", "r") as f:
+     line1= f.readline()
+     line2= f.readline()
+     line3= f.readline()
+     line4= f.readline()
+     line5= f.readline()
+     line6= f.readline()
+     data = f.read()
+     print("line 1", line1)
+     print("line 2", line2)
+     print("line 3", line5)
+     print("line 6", line6)
+     print("data is", data)
 
-# #read all Lines 
-# with open("textfile.txt", "r") as f:
-#  lines = f.readlines()
-#  print(lines)
+#read all Lines 
+with open("textfile.txt", "r") as f:
+ lines = f.readlines()
+ print(lines)
 
-#  #RENAME FILES
-#  import os
-#  os.rename("ex.txt", "xe.txt")
+ #RENAME FILES
+ import os
+ os.rename("ex.txt", "xe.txt")
 
-#  #delete files
-# os.remove("xe.txt")
+ #delete files
+os.remove("xe.txt")
 
 #create folder
 import os
