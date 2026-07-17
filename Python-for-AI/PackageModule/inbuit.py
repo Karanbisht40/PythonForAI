5347aa5a415c87f1bfcfd7178d43635cc1550fc8
Karan Bisht <karanbisht4041@gmail.com>
2026-07-17 23:19:09 +0530

    module

--- Python-for-AI/PackageModule/inbuit.py
+++ Python-for-AI/PackageModule/inbuit.py
@@ -2,13 +2,15 @@
 import math 
 print(math.sqrt(31))
 
+
 #random
 import random
 print(random.randint(1,10))
+
 
 #datetime
 from datetime import datetime
 print(datetime.now())
 
-#pip install <libraries name>
-#import pandas as pd
+# pip install <libraries name>
+# import pandas as pd
